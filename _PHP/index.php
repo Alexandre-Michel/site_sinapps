35aee557cc508f93d94d780496a060db5d4e3dd4
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-04-23 15:25:14 +0200

    Test index + CSS

--- _PHP/index.php
+++ _PHP/index.php
@@ -5,6 +5,11 @@ require_once 'webpage.class.php';
 $p = new WebPage("Accueil");
 
 $p->appendContent(<<<HTML
-<div>J'aime les patates</div>
+<div class = 'content'>
+	<div class = 'msg_welcome'>
+		Bienvenue chez SINAPP'S \n
+		Agence de maintenance informatique dédiée aux professionnels.
+	</div>
+</div>
 HTML
 );
