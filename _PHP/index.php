1776e5c3e0b733c4825493d538448cbab44e003c
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-04-23 16:38:32 +0200

    Re petit test

--- _PHP/index.php
+++ _PHP/index.php
@@ -6,8 +6,8 @@ $p = new WebPage("Accueil");
 
 $p->appendContent(<<<HTML
 <div class = "content">
-	<div class "msg_welcome"
-		<div class = "title welcome">Bienvenue chez SINAPP\'S</div>
+	<div class "msg_welcome">
+		<div class = "title welcome">Bienvenue chez SINAPP'S</div>
 		<div class = "st welcome">Agence de maintenance informatique dédiée aux professionnels.</div>
 	<div>
 </div>
