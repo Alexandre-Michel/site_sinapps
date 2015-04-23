eb9c9ecdf2f53c644d45d5434dafce54577e1a01
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-04-23 15:50:40 +0200

    Test CSS sur index

--- _PHP/index.php
+++ _PHP/index.php
@@ -5,11 +5,11 @@ require_once 'webpage.class.php';
 $p = new WebPage("Accueil");
 
 $p->appendContent(<<<HTML
-<div class = 'content'>
-	<div class = 'msg_welcome'>
-		Bienvenue chez SINAPP'S \n
-		Agence de maintenance informatique dédiée aux professionnels.
-	</div>
+<div class = "content">
+	<div class "msg_welcome"
+		<div class = "title welcome">Bienvenue chez SINAPP\'S</div>
+		<div class = "st welcome">Agence de maintenance informatique dédiée aux professionnels.</div>
+	<div>
 </div>
 HTML
 );
