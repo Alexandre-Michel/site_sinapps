4ef8794d1b36d604425d22fe206c4ad92058071e
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-04-24 11:01:08 +0200

    PHP Index

--- _PHP/index.php
+++ _PHP/index.php
@@ -10,6 +10,37 @@ $p->appendContent(<<<HTML
 		<div class = "title welcome">Bienvenue chez SINAPP'S</div>
 		<div class = "st welcome">Agence de maintenance informatique dédiée aux professionnels.</div>
 	<div>
+	<div class = "intro">
+		<h2>
+			<span class = "titre_intro">Que faisons-nous ?</span>
+		</h2>
+	</div>
+	<div class = "intro_box">
+		<div class = "presta box1">
+			<h3>Presta 1</h3>
+			<div class = "txt_box">Ceci est un texte de test MODEFOCKER</div>
+		</div>
+		<div class = "presta box2">
+			<h3>Presta 2</h3>
+			<div class = "txt_box">Ceci est un texte de test MODEFOCKER</div>
+		</div>
+		<div class = "presta box3">
+			<h3>Presta 3</h3>
+			<div class = "txt_box">Ceci est un texte de test MODEFOCKER</div>
+		</div>
+		<div class = "presta box4">
+			<h3>Presta 4</h3>
+			<div class = "txt_box">Ceci est un texte de test MODEFOCKER</div>
+		</div>
+		<div class = "presta box5">
+			<h3>Presta 5</h3>
+			<div class = "txt_box">Ceci est un texte de test MODEFOCKER</div>
+		</div>
+		<div class = "presta box6">
+			<h3>Presta 6</h3>
+			<div class = "txt_box">Ceci est un texte de test MODEFOCKER</div>
+		</div>
+	</div>
 </div>
 HTML
 );
