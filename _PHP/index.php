556e8d0943ed85f034a828acce17ad687dbc3575
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-04-25 10:44:44 +0200

    Modif taille box

--- _PHP/index.php
+++ _PHP/index.php
@@ -21,7 +21,9 @@ $p->appendContent(<<<HTML
 		<div class = "box_container">
 			<div class = "presta box1">
 				<div class = "th3">Presta 1</div>
-				<img id="logo_ordi" src="../_IMG/ordi.png" alt="logo1"/>
+				<div class = "img_presta">
+					<img id="logo_ordi" src="../_IMG/ordi.png" alt="logo1"/>
+				</div>
 				<div class = "txt_box">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare arcu nunc, sit amet consectetur ipsum tempor ut. Duis facilisis cursus faucibus. Morbi vehicula elit sit amet blandit fringilla. Etiam ut consequat eros. Sed quis tortor elementum, </div>
 				<div class = "more">
 					<a href="">En savoir plus</a>
