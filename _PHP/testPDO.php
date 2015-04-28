a146f51cff507e48ea5cb87c3d55b367e612101f
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-04-28 10:42:15 +0200

    Test PDO

---
+++ _PHP/testPDO.php
@@ -0,0 +1,15 @@
+myPDO::setConfiguration('mysql:host=mysql;dbname=sinapps;charset=utf8', 'root', '');
+
+$pdo = myPDO::getInstance() ;
+
+$stmt = $pdo->prepare(<<<SQL
+    SELECT *
+    FROM divers
+SQL
+);
+
+$stmt->execute() ;
+
+while (($ligne = $stmt->fetch()) !== false) {
+    echo "<p>{$ligne['name']}\n" ;
+}
