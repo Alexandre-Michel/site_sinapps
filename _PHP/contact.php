edc868d461106f07a61cde3e7707d4c67b309201
Alexandre <alexandre.michel@sinapps.fr>
2015-04-29 17:59:32 +0200

    Ajout de div dans contact

--- _PHP/contact.php
+++ _PHP/contact.php
@@ -6,15 +6,23 @@ $p = new WebPage("Contact");
 
 $p->appendContent(<<<HTML
 	<div class="content">
+		<div class="form_contact">
 		<form method="post" action="mailto:alexandre.michel@etudiant.univ-reims.fr">
-			<input type="text" name="nom" placeholder="Votre nom"> 
-			<input type="text" name="prenom" placeholder="Votre prénom">
-			<input type="email" name="mail" placeholder="Votre adresse mail">
-			<input type="tel" name="telephone" placeholder="Votre téléphone">
-			<input type="text" name="societe" placeholder="Votre société">
-			<textarea rows=5 name="message">Votre message</textarea>
-			<input type="submit" value="Envoyer">
+			<div class="champs_contact">
+				<input type="text" name="nom" placeholder="Votre nom"> 
+				<input type="text" name="prenom" placeholder="Votre prénom">
+				<input type="email" name="mail" placeholder="Votre adresse mail">
+				<input type="tel" name="telephone" placeholder="Votre téléphone" pattern="[0][0-9]{9}">
+				<input type="text" name="societe" placeholder="Votre société">
+			</div>
+			<div class="msg_contact">
+				<textarea rows=5 name="message" placeholder="Votre message"></textarea>
+			</div>
+			<div class="envoi_contact">	
+				<input type="submit" value="Envoyer">
+			</div>
 		</form>
+		</div>
 	</div>
 HTML
 );
