1e1f117c3452fb694f4fd625a38fc2baa60a40a0
Alexandre <alexandre.michel@sinapps.fr>
2015-04-29 11:10:58 +0200

    test

--- _PHP/myPDO.include.php
+++ _PHP/myPDO.include.php
@@ -2,4 +2,4 @@
 
 include 'myPDO.class.php';
 
-myPDO::setConfiguration('mysql:host=127.0.0.1;port=3337;dbname=sinapps;charset=utf8', 'root', '');
+myPDO::setConfiguration('mysql:host=192.168.110.4;port=3337;dbname=sinapps;charset=utf8', 'root', '');
