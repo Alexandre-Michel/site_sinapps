d569c901086be6935d3ea550784756dbd4d88af5
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-04-29 15:48:36 +0200

    modif sql

--- _PHP/box_container.php
+++ _PHP/box_container.php
@@ -5,6 +5,7 @@ $pdo = myPDO::getInstance();
 $stmt = $pdo->prepare(<<<SQL
 	SELECT *
 	FROM TYPE_PRESTATION
+	WHERE id_type_prestation = 1
 SQL
 );
 
@@ -17,12 +18,12 @@ function createBox($i)
 	return <<< HTML
 	<div class = "box_container">
 			<div class = "presta box1">
-				<div class = "th3">{$tableau['nom_prestation'][$i]}</div>
+				<div class = "th3">{$tableau['nom_prestation']}</div>
 				<div class = "img_presta">
-					<img id="logo_ordi" src="{$tableau['path_prestation'][$i]}" alt="logo1"/>
+					<img id="logo_ordi" src="{$tableau['path_prestation']" alt="logo1"/>
 				</div>
 				<div class = "border_logo"></div>
-				<div class = "txt_box">{$tableau['description_prestation'][$i]}</div>
+				<div class = "txt_box">{$tableau['description_prestation']}</div>
 				<div class = "more">
 					<a href="">En savoir plus &rsaquo;</a>
 				</div>
