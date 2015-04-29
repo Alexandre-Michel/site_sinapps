b87c4c59ee6889f2226144e14bafa51762e87012
sinapps-server <Hades_OC@sinapps.fr>
2015-04-29 14:28:52 +0200

    connexion pdo

--- _PHP/myPDO.include.php
+++ _PHP/myPDO.include.php
@@ -2,4 +2,4 @@
 
 include 'myPDO.class.php';
 
-myPDO::setConfiguration('mysql:host=syn-sgbd;dbname=baseTest;', 'admin', 'test123');
+myPDO::setConfiguration('mysql:host=syn-sgbd:3337;dbname=sinapps;', 'web1', 'k4fg5s4F1T');
