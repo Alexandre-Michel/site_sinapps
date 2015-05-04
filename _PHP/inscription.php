a279dd5a1bbef30de956139efa93ef5a52a80703
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-05-04 16:23:15 +0200

    Inscription

---
+++ _PHP/inscription.php
@@ -0,0 +1,100 @@
+<?php
+
+require_once 'webpage.class.php';
+require_once 'myPDO.include.php';
+
+$pdo = myPDO::getInstance();
+
+$stmt = $pdo->prepare(<<<SQL
+	SELECT count(*)
+	FROM PERSONNE
+	WHERE mail_personne = :mail
+SQL
+);
+
+$stmt->bindValue(':mail', $_POST['mail']);
+$stmt->execute();
+$count = $stmt->fetch();
+
+$p = new WebPage("Inscription | Sinapp's");
+
+//Soumission du formulaire
+if(isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription')
+{
+	//Existence des variables
+	if((isset($_POST['mail']) && !empty($_POST['mail'])) && (isset($_POST['password']) && !empty($_POST['password'])) && (isset($_POST['confirm']) && !empty($_POST['confirm'])))
+	{
+		//Correspondance des mots de passe
+		if($_POST['password'] == $_POST['confirm'])
+		{
+			//Vérification de la longueur du mot de passs (6 caractères au minimum)
+			if(strlen($_POST['password']) >= 6)
+			{
+				//Adresse mail valide ?
+				if(preg_match([-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}, $_POST['mail']))
+				{
+					//Vérification qu'il n'y a pas déjà un utilisateur avec le même mail
+					if($count == 0)
+					{
+						//Création de l'utilisateur via son nom, prenom, mail et mot de passe
+						createPersonne($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['password']);
+						$form = false;
+					}
+					//Adresse mail déjà utilisée
+					else
+					{
+						$message = "L'adresse mail que vous avez rentrée est déjà utilisée.";
+						$form = true;
+					}
+				}
+				//Adresse mail non valide
+				else
+				{
+					$message = "L'adresse mail que vous avez rentrée n'est pas valide.";
+					$form = true;
+				}
+			}
+			//Mot de passe trop court (moins de 6 caractères)
+			else
+			{
+				$message = "Le mot de passe que vous avez saisi n'est pas assez long (6 caractères minimum).";
+				$form = true;
+			}
+		}
+		//Les mot de passe et la confirmation ne coïncident pas
+		else
+		{
+			$message = "Les mots de passes saisis ne sont pas identiques.";
+			$form = true;
+		}
+	}
+}
+
+if($form)
+{
+    //On affiche un message sil y a lieu
+    if(isset($message))
+    {
+            echo '<div class="message">'.$message.'</div>';
+    }
+    //On affiche le formulaire
+	$p->appendContent(<<<HTML
+		<div class="content">
+		    <form action="inscription.php" method="post">
+		        Veuillez remplir ce formulaire pour vous inscrire:<br/>
+		        <div class="center">
+		            <label for="nom">Nom d'utilisateur</label>
+		            	<input type="text" name="nom" value="<?php if(isset($_POST['nom'])){echo htmlentities($_POST['nom'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
+		            <label for="prenom">Nom d'utilisateur</label>
+		            	<input type="text" name="prenom" value="<?php if(isset($_POST['prenom'])){echo htmlentities($_POST['prenom'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
+		            <label for="password">Mot de passe<span class="small">(6 caract&egrave;res min.)</span></label><input type="password" name="password" /><br />
+		            <label for="confirm">Mot de passe<span class="small">(v&eacute;rification)</span></label><input type="password" name="confirm" /><br />
+		            <label for="mail">Email</label>
+		            	<input type="text" name="mail" value="<?php if(isset($_POST['mail'])){echo htmlentities($_POST['mail'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
+		            <input type="submit" value="Envoyer" />
+		        </div>
+		    </form>
+		</div>
+HTML
+;)
+}
