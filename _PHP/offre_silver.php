6244eaa4b152e3cb21a94e319fd709a297d7e56c
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-05-04 11:20:34 +0200

    Test

--- _PHP/offre_silver.php
+++ _PHP/offre_silver.php
@@ -39,45 +39,45 @@ while (($ligne = $stmt->fetch()) !== false) {
 $p = new WebPage("Agence");
 
 $p->appendContent(<<<HTML
-	<div class="content">
-		<div class="offre_box">
-			<div class="box_offre">
-				<div class="offre">
-					<div class = "th3">{$offre['nom_offre']}</div>
-					<div class = "img_offre">
-						<img id="logo_offre" src="{$offre['path_logo']}" alt="logo1"/>
-					</div>
-					<div class = "border_logo"></div>
-					<div class = "txt_box">{$offre['desc_page']}</div>
+<div class="content">
+	<div class="offre_box">
+		<div class="box_offre">
+			<div class="offre">
+				<div class = "th3">{$offre['nom_offre']}</div>
+				<div class = "img_offre">
+					<img id="logo_offre" src="{$offre['path_logo']}" alt="logo1"/>
 				</div>
+				<div class = "border_logo"></div>
+				<div class = "txt_box">{$offre['desc_page']}</div>
 			</div>
 		</div>
-		</div class = "intro_box">
+	</div>
+	</div class = "intro_box">
 HTML
 );
 for($i = 0; $i < 6; $i++)
 {
-	$p->appendContent(<<<HTML
-			<div class = "box_container">
-				<div class = "presta">
-					<div class = "th3">{$liste_noms[$i]}</div>
-					<div class = "img_presta">
-						<img id="logo_ordi" src="{$liste_img[$i]}" alt="logo1"/>
-					</div>
-					<div class = "border_logo"></div>
-					<div class = "txt_box">{$liste_description[$i]}</div>
-					<div class = "more">
-						<a href="">En savoir plus &rsaquo;</a>
-					</div>
+	$p->appendContent(<<< HTML
+		<div class = "box_container">
+			<div class = "presta">
+				<div class = "th3">{$liste_noms[$i]}</div>
+				<div class = "img_presta">
+					<img id="logo_ordi" src="{$liste_img[$i]}" alt="logo1"/>
+				</div>
+				<div class = "border_logo"></div>
+				<div class = "txt_box">{$liste_description[$i]}</div>
+				<div class = "more">
+					<a href="">En savoir plus &rsaquo;</a>
 				</div>
 			</div>
+		</div>
 HTML
 	);
 }
 		
 $p->appendContent(<<<HTML
-		</div>
 	</div>
+</div>
 HTML
 );
 
