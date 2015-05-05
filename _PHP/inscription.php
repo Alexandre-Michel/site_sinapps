0286016e5ccf40f48375adf964c77adc3b5382bc
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-05-05 11:22:57 +0200

    Work !!

--- _PHP/inscription.php
+++ _PHP/inscription.php
@@ -39,7 +39,7 @@ if(isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription')
 					if($count["nbre"] == 0)
 					{
 						//Création de l'utilisateur via son nom, prenom, mail et mot de passe
-						createPersonne($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['password']);
+						Personne::createPersonne($_POST['nom'], $_POST['prenom'], $_POST['mail'], $_POST['password']);
 						$form = false;
 					}
 					//Adresse mail déjà utilisée
