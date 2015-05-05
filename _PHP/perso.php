4d1d93acd9730bda6d13804784fd0daf53fe2ec5
Alexandre <alexandre.michel@sinapps.fr>
2015-05-05 11:08:34 +0200

    test page perso

--- _PHP/perso.php
+++ _PHP/perso.php
@@ -1,9 +1,10 @@
 <?php
 
+require_once 'webpage.class.php';
 require_once 'Personne.class.php';
 
 Personne::checkConnected(); //Redirige vers connexion si non connecté !
-$user = Personne::getCurrentUser();
+//$user = Personne::getCurrentUser();
 
 $p = new WebPage("Espace personnel | Sinapp's");
 
