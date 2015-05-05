b365da371837e63e2ce979b2655928c5ecae0f2d
sinapps-server <Hades_OC@sinapps.fr>
2015-05-05 21:41:48 +0200

    f

--- _PHP/perso.php
+++ _PHP/perso.php
@@ -4,6 +4,7 @@ require_once 'webpage.class.php';
 require_once 'Personne.class.php';
 
 $p = new WebPage("Espace personnel - Sinapp's");
+//$p->appendJs(console.log(Personne::isConnected()));
 
 
 Personne::checkConnected();
