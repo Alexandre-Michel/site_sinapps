95890226b8da4a2857bb1192557d67046d4aa4a4
Alexandre <alexandre.michel@sinapps.fr>
2015-05-07 14:45:14 +0200

    incident

---
+++ _PHP/incident.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once 'webpage.class.php';
+require_once 'Personne.class.php';
+require_once 'incident.class.php';
+
+$p = new WebPage("Détail de l'incident - Sinapp's");
+
+try {
+	$user = Personne::createFromSession();
+	if (isset($_GET['i']) && $_GET['i'] != "") {
+		$incident = Incident::createFromId($_GET['i']);
+		if ($incident->getIdPersonne() == $user->getIdPers()) {
+			$p->appendContent(<<<HTML
+				<div class="content">
+					{$incident->getNomIncident()}
+					{$incident->getDescriptionIncident()}
+					{$incident->getDateIncident()}	
+				</div>
+HTML
+			);				
+		}
+		else {
+			header('location: ./index.php');
+			exit;
+		}
+	}
+	else {
+		header('location: ./incidents.php');
+		exit;
+	}
+}
+catch (notInSessionException $e) {
+    // Pas d'utilisateur connecté
+    header("Location: ./connexion.php") ;
+    die() ;
+}
+
+echo $p->toHTML();
