fd9d08ce970c1c11e0173bcfada9522211d344ae
Alexandre <alexandre.michel@sinapps.fr>
2015-05-07 16:29:10 +0200

    form

--- _PHP/newIncident.php
+++ _PHP/newIncident.php
@@ -11,7 +11,29 @@ try {
 
     $p->appendContent(<<<HTML
     	<div class="content">
-    		
+    		<input type="text" required placeholder="Nom de l'incident" name="nom"/><br/>
+			<textarea required row=8 placeholder="Description de votre incident" name="desc"></textarea><br/>
+			<select name="type">
+HTML
+	);
+
+	$stmt = myPDO::getInstance()->prepare(<<<SQL
+		SELECT *
+		FROM TYPE_INCIDENT
+SQL
+	);	
+	$stmt->setFetchMode(PDO::FETCH_CLASS, __CLASS__);	
+	$stmt->execute();
+	$arrayType = $stmt->fetchAll();
+
+	foreach ($arrayType as $unType) {
+		$p->appendContent("<option selected name=\"{$unType->id_type_incident}\">{$unType->type_incident}</option>");
+	}
+
+	$p->appendContent(<<<HTML
+			</select>
+			<input type="submit" value="Soumettre">
+    	</div>	
 HTML
     );
 }
