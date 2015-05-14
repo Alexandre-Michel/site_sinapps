7d842eef3c24bad67da399964455d265a1611d93
Alexandre <alexandre.michel08@gmail.com>
2015-05-14 14:32:46 +0200

    MAJ info

--- _PHP/Type_incident.class.php
+++ _PHP/Type_incident.class.php
@@ -15,4 +15,20 @@ class Type_incident {
 	public function getDescType() {
 		return $this->description_type_incident;
 	}
+
+	public static function createTypeIncidentFromId($id) {
+		$stmt = myPDO::getInstance()->prepare(<<<SQL
+			SELECT * 
+			FROM TYPE_INCIDENT
+			WHERE id_type_incident = :id
+SQL
+		);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, __CLASS__);
+		$stmt->bindValue(":id", $id);
+		$stmt->execute();
+		if (($object = $stmt->fetch()) !== false)
+			return $object;
+		else
+			throw new Exception ("Type incident not found");
+	}
 }
