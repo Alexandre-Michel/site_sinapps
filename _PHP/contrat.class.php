6ec5f46ce7ab1c2e26a8ed568be0657d25d279e2
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-05-15 16:09:36 +0200

    strtotime est

--- _PHP/contrat.class.php
+++ _PHP/contrat.class.php
@@ -143,10 +143,7 @@ HTML;
 			$status = "";
 			$dateFV = date('d-m-Y', strtotime($ctr->getFinValidite()));
 			$dateSign = date('d-m-Y', strtotime($ctr->getDateSignContrat()));
-
-			$dtFV = new DateTime($dateFV);
-			$today = new DateTime(date("d-m-Y"));
-			if ($dtFv > $today)
+			if (strtotime($dateFV) > strtotime(date("d-m-Y")))
 			{
 				$status = "<div class = \"status t\">Contrat en cours de validité</div>";
 			}
