5a914188771e3ef22f0c50a106ae8caca2fe372a
sinapps-server <Hades_OC@sinapps.fr>
2015-05-19 16:46:07 +0200

    correct css

--- _PHP/habilitations.php
+++ _PHP/habilitations.php
@@ -41,9 +41,6 @@ try {
 			else if($_REQUEST['msg'] == 2) {
 				$msg = "<div class='rate'>Echec, veuillez réessayer.</div>";
 			}
-			else if($_REQUEST['msg'] == 3) {
-				$msg = "<div class='succes'>Habilitation modifiée avec succès.</div>";
-			}
 		}
 
 		$p->appendContent(<<<HTML
@@ -55,7 +52,8 @@ try {
 				<div class = "th2">
 					Il y a actuellement {$nombre} habilitation(s)
 				</div>
-				<div class= "box1">
+				<div class = "intro_box">
+					<div class= "box1">
 					
 HTML
 		);
@@ -71,19 +69,20 @@ HTML
 			}
 		}
 			$p->appendContent(<<<HTML
-					<div class = "row bordure fond">
-							<div>{$uneHab->getNomHab()}</div>
-							<div class="boutons_objet">
-								<input class="button" type="submit" onclick="modifier({$uneHab->getIdHab()})" value="Modifier">
-								<input class="button" type="submit" onclick="effacer({$uneHab->getIdHab()})" value="Supprimer">
-							</div>	
-						
-					</div>	
+						<div class = "row fond">
+								<div>{$uneHab->getNomHab()}</div>
+								<div class="boutons_objet">
+									<input class="button" type="submit" onclick="modifier({$uneHab->getIdHab()})" value="Modifier">
+									<input class="button" type="submit" onclick="effacer({$uneHab->getIdHab()})" value="Supprimer">
+								</div>	
+							
+						</div>	
 HTML
 			);
 		}
 
 		$p->appendContent(<<<HTML
+					</div>
 				</div>
 				<button type="submit" onclick="history.back()" class="button">Retour</button>
 			</div>	
