cf754dbc75b720607078aaa7aa28007aeb9d1885
Benjamin <benjamin.jacques@etudiant.univ-reims.fr>
2015-05-21 10:29:10 +0200

    Petit Test

--- _PHP/parcs.php
+++ _PHP/parcs.php
@@ -13,54 +13,55 @@ $p = new WebPage("Parcs - Sinapp's");
 try {
     // Lecture depuis les données de session
     $user = Personne::createFromSession();
-    
-    if ($user->estHabilite()) {
+    switch($user->getIdHabilitation())
+    {
+		case 1: //Admin
+		$droit = 1;
+		break;
+		case 2: // Utilisateur
+		$droit = 2;
+		break;
+    }
 
-    	$msg = "";
+	$msg = "";
 
-    	if(isset($_REQUEST['i'])) {
-    		if(isset($_REQUEST['delete'])) {
-    			try {
-	    			Entreprise::deleteEntreprise($_REQUEST['i']);
-	    			header("location: ./parcs.php?msg=1");
-	    			exit;
-	    		}
-	    		catch (Exception $e) {
-	    			header("location: ./parcs.php?msg=2");
-	    			exit;	    			
-	    		}
+	if(isset($_REQUEST['i'])) {
+		if(isset($_REQUEST['delete'])) {
+			try {
+    			Parc::deleteParc($_REQUEST['i']);
+    			header("location: ./parcs.php?msg=1");
+    			exit;
     		}
+    		catch (Exception $e) {
+    			header("location: ./parcs.php?msg=2");
+    			exit;	    			
+    		}
+		}
 
-    	}
-
-    	if(isset($_REQUEST['msg']))
-    	{
-    		if($_REQUEST['msg'] == 1)
-    			$msg = "<div class='succes'>Parc supprimé avec succès.</div>";
-    		else if($_REQUEST['msg'] == 2)
-    			$msg = "<div class='rate'>Echec, veuillez réessayer.</div>";
-    	}
-
-		$p->appendContent(<<<HTML
-			<div class="content">
-				<div class = "th1">Liste des Parcs</div>
-				{$msg}
-HTML
-		);
-
-		$p->appendContent(Parc::getParcByEntreprise($_GET['i']));
-
-		$p->appendContent(<<<HTML
-				<input type="button" name="retour" value="Retour" onclick="history.back()">
-			</div>
-HTML
-		);
 	}
 
-	else {
-		header('location: ./index.php');
-		exit;
+	if(isset($_REQUEST['msg']))
+	{
+		if($_REQUEST['msg'] == 1)
+			$msg = "<div class='succes'>Parc supprimé avec succès.</div>";
+		else if($_REQUEST['msg'] == 2)
+			$msg = "<div class='rate'>Echec, veuillez réessayer.</div>";
 	}
+
+	$p->appendContent(<<<HTML
+		<div class="content">
+			<div class = "th1">Liste des Parcs</div>
+			{$msg}
+HTML
+	);
+
+	$p->appendContent(Parc::getParcByEntreprise($_GET['i']));
+
+	$p->appendContent(<<<HTML
+			<input type="button" name="retour" value="Retour" onclick="history.back()">
+		</div>
+HTML
+	);
 }
 
 catch (notInSessionException $e) {
